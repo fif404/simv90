c0567b1971503ff507da837ee3735d4b633f4592
fif <fif@DESKTOP-BFHUIKE>
2022-12-29 07:19:49 +1200

    Добавьте файлы проекта.

---
+++ simv90/Program.cs
@@ -0,0 +1,13 @@
+﻿//Скрипников 17 вариант 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int sBefore = str.IndexOf('$');
+            Console.WriteLine($"до него: {sBefore}");
+            int sAfter = str.Length - sBefore - 1;
+            Console.WriteLine($"после него: {sAfter}");
+            Console.ReadLine();
+        }
+    }
